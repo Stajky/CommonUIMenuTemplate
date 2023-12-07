a6bb13db840759478d67248f0a66f1a164f2ac7a
Stajky <stajky@gmail.com>
2023-12-07 09:37:14 +0100

    init commit

---
+++ Source/CommonUIMenuTemplate/CommonUIMenuTemplate.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CommonUIMenuTemplate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CommonUIMenuTemplate, "CommonUIMenuTemplate" );
