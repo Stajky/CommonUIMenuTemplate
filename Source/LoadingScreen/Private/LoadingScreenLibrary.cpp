6e6500ad60140a0f78ac450b8e86af8c85ec9993
Stajky <stajky@gmail.com>
2023-12-28 21:33:25 +0100

    add option to add Blank space to spacer for begining, end and option to set blank space before tite. cleanup, Add Stop loading screen function to loading screen module library, fix uncompiled code issue that made credits data table category empty

--- Source/LoadingScreen/Private/LoadingScreenLibrary.cpp
+++ Source/LoadingScreen/Private/LoadingScreenLibrary.cpp
@@ -10,5 +10,17 @@ void ULoadingScreenLibrary::StartLoadingScreen(const FLevelLoadingScreenSettings
 	FLoadingScreenAttributes LSAttributes;
 	LSAttributes.MinimumLoadingScreenDisplayTime = MinimumLoadingScreenDisplayTime;
 	LSAttributes.WidgetLoadingScreen = SNew(SLevelLoadingScreen).Settings(Settings);
+	// Here is an option to basically take control when the loading screen ends
+	// By default the loading screen will end when level is loaded it might be a good idea to use this if
+	// we need to wait for some other async function as well (I dont know loading a save file or something)
+	// Currently its not used RN since I assume that the level load will take more time than other things, but
+	// in the future I might need to revisit this. Good luck future someone
+	// LSAttributes.bWaitForManualStop = true;
 	GetMoviePlayer()->SetupLoadingScreen(LSAttributes);
+	
+}
+
+void ULoadingScreenLibrary::StopLoadingScreen()
+{
+	GetMoviePlayer()->StopMovie();
 }
