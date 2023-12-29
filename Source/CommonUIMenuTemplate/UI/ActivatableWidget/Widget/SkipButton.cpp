0319bbbd2a75731436cf831fc020c96b2a41f2ba
Stajky <stajky@gmail.com>
2023-12-29 14:49:52 +0100

    Skip Button for credit screen

---
+++ Source/CommonUIMenuTemplate/UI/ActivatableWidget/Widget/SkipButton.cpp
@@ -0,0 +1,85 @@
+﻿// 
+
+
+#include "SkipButton.h"
+#include "CommonUITypes.h"
+#include "Input/CommonUIInputTypes.h"
+
+void USkipButton::SetListeningForInput(bool bShouldListen)
+{
+	if (GetUISubsystem() == nullptr)
+	{
+		// Shutting down
+		return;
+	}
+
+	if (bShouldListen != bIsListeningForInput)
+	{
+		bIsListeningForInput = bShouldListen;
+		UpdateBindings();
+	}
+}
+
+void USkipButton::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (bAutoListenForInput)
+	{
+		SetListeningForInput(true);
+	}
+}
+
+void USkipButton::NativeDestruct()
+{
+	SetListeningForInput(false);
+	Super::NativeDestruct();
+}
+
+void USkipButton::UpdateBindings()
+
+{	// New input system binding flow
+	if (bIsListeningForInput)
+	{
+		const bool bIsEnhancedInputSupportEnabled = CommonUI::IsEnhancedInputSupportEnabled();
+		if (bIsEnhancedInputSupportEnabled && EnhancedInputAction)
+		{
+			FBindUIActionArgs args = FBindUIActionArgs(EnhancedInputAction, false,
+			                                           FSimpleDelegate::CreateUObject(this, &USkipButton::SkipButtonPressedNative));
+			args.KeyEvent = IE_Pressed;
+			InputActionHandlePressed = RegisterUIActionBinding(args);
+
+			args = FBindUIActionArgs(EnhancedInputAction, false,
+			 	FSimpleDelegate::CreateUObject(this, &USkipButton::SkipButtonReleasedNative));
+			args.KeyEvent = IE_Released;
+			InputActionHandleReleased = RegisterUIActionBinding(args);
+		}
+		else
+		{
+			FBindUIActionArgs args = FBindUIActionArgs(InputAction, false,
+			                                           FSimpleDelegate::CreateUObject(this, &USkipButton::SkipButtonPressedNative));
+			args.KeyEvent = IE_Pressed;
+			InputActionHandlePressed = RegisterUIActionBinding(args);
+
+			args = FBindUIActionArgs(InputAction, false,
+				FSimpleDelegate::CreateUObject(this, &USkipButton::SkipButtonReleasedNative));
+			args.KeyEvent = IE_Released;
+			InputActionHandleReleased = RegisterUIActionBinding(args);
+		}
+	}
+	else
+	{
+		InputActionHandlePressed.Unregister();
+		InputActionHandleReleased.Unregister();
+	}
+}
+
+void USkipButton::SkipButtonReleasedNative() const
+{
+	OnSkipButtonReleased.Broadcast();
+}
+
+void USkipButton::SkipButtonPressedNative() const
+{
+	OnSkipButtonPressed.Broadcast();
+}
---
+++ Source/CommonUIMenuTemplate/UI/ActivatableWidget/Widget/SkipButton.h
@@ -0,0 +1,73 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+
+
+#include "Engine/DataTable.h"
+#include "SkipButton.generated.h"
+
+class UInputAction;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSkipButtonPressed);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSkipButtonReleased);
+// DECLARE_DELEGATE(FSkipButtonPressed);
+// DECLARE_DELEGATE(FSkipButtonReleased);
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, ClassGroup = UI, meta = (Category = "CmtUI", DisableNativeTick))
+class COMMONUIMENUTEMPLATE_API USkipButton : public UCommonUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = TabList)
+	virtual void SetListeningForInput(bool bShouldListen);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_HandleInputAction();
+
+
+protected:
+	// UUserWidget interface
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	// End UUserWidget
+	
+private:
+	void UpdateBindings();
+
+	void SkipButtonPressedNative() const;
+	void SkipButtonReleasedNative() const;
+	
+protected:
+	/** Whether to register input immediately upon construction */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = SkipButton, meta = (ExposeOnSpawn = "true"))
+	bool bAutoListenForInput;
+
+	/** The input action to listen for causing the skip */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = SkipButton, meta = (RowType = "/Script/CommonUI.CommonInputActionDataBase"))
+	FDataTableRowHandle InputAction;
+
+	/** The input action to listen for causing the skip */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = SkipButton, meta = (EditCondition = "CommonInput.CommonInputSettings.IsEnhancedInputSupportEnabled", EditConditionHides))
+	TObjectPtr<UInputAction> EnhancedInputAction;
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FSkipButtonPressed OnSkipButtonPressed;
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FSkipButtonReleased OnSkipButtonReleased;
+	
+private:
+	bool bIsListeningForInput = false;
+
+	FUIActionBindingHandle InputActionHandlePressed;
+	FUIActionBindingHandle InputActionHandleReleased
+	;
+};
