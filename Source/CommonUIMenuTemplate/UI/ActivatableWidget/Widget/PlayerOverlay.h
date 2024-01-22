e5b455d5850250254e7e6334d7a372805a92d024
Stajky <stajky@gmail.com>
2024-01-22 12:15:04 +0100

    moved assets to asset folder, created base PlayerOverlay class, Created Progress Globes, Created Spell Globes

---
+++ Source/CommonUIMenuTemplate/UI/ActivatableWidget/Widget/PlayerOverlay.h
@@ -0,0 +1,16 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUIMenuTemplate/UI/ActivatableWidget/CmtActivatableWidget_WithController.h"
+#include "PlayerOverlay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMONUIMENUTEMPLATE_API UPlayerOverlay : public UCmtActivatableWidget_WithController
+{
+	GENERATED_BODY()
+};
