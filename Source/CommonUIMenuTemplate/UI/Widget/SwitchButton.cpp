66810b51604bce6235b364e2d6f7794ee061e0d2
Stajky <stajky@gmail.com>
2024-02-07 11:38:16 +0100

    Add Switch Button (button can highlight the chosen option, automaticaly works for controller using navigation), add options pins material (creates texture that shows selected, default, inactive value bars used in Switch Button)

---
+++ Source/CommonUIMenuTemplate/UI/Widget/SwitchButton.cpp
@@ -0,0 +1,141 @@
+﻿
+#include "SwitchButton.h"
+#include "CommonTextBlock.h"
+
+
+USwitchButton::USwitchButton(const FObjectInitializer& ObjectInitializer)
+{
+	SetIsFocusable(true);
+}
+
+bool USwitchButton::Initialize()
+{
+	if (Super::Initialize())
+	{
+		OnNavigation.BindUObject(this, &USwitchButton::HandleNavigation);
+
+		return true;
+	}
+
+	return false;
+}
+
+FNavigationReply USwitchButton::NativeOnNavigation(const FGeometry& MyGeometry, const FNavigationEvent& InNavigationEvent, const FNavigationReply& InDefaultReply)
+{
+	switch (InNavigationEvent.GetNavigationType())
+	{
+	case EUINavigation::Left:
+	case EUINavigation::Right:
+		return FNavigationReply::Custom(OnNavigation);
+	default:
+		return InDefaultReply;
+	}
+}
+
+TSharedPtr<SWidget> USwitchButton::HandleNavigation(EUINavigation UINavigation)
+{
+	if (UINavigation == EUINavigation::Left)
+	{
+		ShiftTextLeftInternal(true);
+	}
+	else if (UINavigation == EUINavigation::Right)
+	{
+		ShiftTextRightInternal(true);
+	}
+
+	return nullptr;
+}
+
+void USwitchButton::PopulateTextLabels(TArray<FText> Labels)
+{
+	TextLabels = Labels;
+	SelectedIndex = 0;
+
+	BP_OnOptionsPopulated(TextLabels.Num());
+}
+
+FText USwitchButton::GetSelectedText() const
+{
+	return LabelText->GetText();
+}
+
+void USwitchButton::SetSelectedItem(int32 InIndex)
+{
+	if (TextLabels.IsValidIndex(InIndex))
+	{
+		SelectedIndex = InIndex;
+		LabelText->SetText(TextLabels[SelectedIndex]);
+
+		BP_OnOptionSelected(SelectedIndex);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Trying to set CommonRotator to an out of bounds index: %i"), InIndex);
+	}
+}
+
+void USwitchButton::ShiftTextLeft()
+{
+	ShiftTextLeftInternal(false);
+}
+
+void USwitchButton::ShiftTextLeftInternal(bool bFromNavigation)
+{
+	if (IsInteractionEnabled())
+	{
+		if (SelectedIndex <= 0)
+		{
+			SelectedIndex = TextLabels.Num() - 1;
+		}
+		else
+		{
+			SelectedIndex--;
+		}
+
+		if (TextLabels.IsValidIndex(SelectedIndex))
+		{
+			SetSelectedItem(SelectedIndex);
+
+			if (OnSwitched.IsBound())
+			{
+				OnSwitched.Broadcast(SelectedIndex);
+			}
+
+			OnSwitchedEvent.Broadcast(SelectedIndex, bFromNavigation);
+		}
+	}
+}
+
+void USwitchButton::ShiftTextRight()
+{
+	ShiftTextRightInternal(false);
+}
+
+void USwitchButton::ShiftTextRightInternal(bool bFromNavigation)
+{
+	if (IsInteractionEnabled())
+	{
+		if (SelectedIndex >= TextLabels.Num() - 1)
+		{
+			SelectedIndex = 0;
+		}
+		else
+		{
+			SelectedIndex++;
+		}
+
+		if (TextLabels.IsValidIndex(SelectedIndex))
+		{
+			SetSelectedItem(SelectedIndex);
+
+			if (OnSwitched.IsBound())
+			{
+				OnSwitched.Broadcast(SelectedIndex);
+			}
+
+			OnSwitchedEvent.Broadcast(SelectedIndex, bFromNavigation);
+		}
+	}
+}
+
+
---
+++ Source/CommonUIMenuTemplate/UI/Widget/SwitchButton.h
@@ -0,0 +1,87 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "SwitchButton.generated.h"
+
+
+class UCommonTextBlock;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSwitched, int32, Value);
+
+
+/**
+ * A class that has the ability to switch between text array and simulates the small buttons select bar by texture 
+ */
+UCLASS(meta = (DisableNativeTick))
+class COMMONUIMENUTEMPLATE_API USwitchButton : public UCommonButtonBase
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	virtual bool Initialize() override;
+	virtual FNavigationReply NativeOnNavigation(const FGeometry& MyGeometry, const FNavigationEvent& InNavigationEvent, const FNavigationReply& InDefaultReply) override;
+
+	/** Handle and use controller navigation to rotate text */
+	FNavigationDelegate OnNavigation;
+	TSharedPtr<SWidget> HandleNavigation(EUINavigation UINavigation);
+
+	/** Set the array of texts available */
+	UFUNCTION(BlueprintCallable, Category = "Behavior")
+	void PopulateTextLabels(TArray<FText> Labels);
+
+	/** Gets the current text value of the slider. */
+	UFUNCTION(BlueprintCallable, Category = "Behavior")
+	FText GetSelectedText() const;
+
+	/** Sets the current value of the slider. */
+	UFUNCTION(BlueprintCallable, Category = "Behavior")
+	virtual void SetSelectedItem(int32 InValue);
+
+	/** Gets the current selected index */
+	UFUNCTION(BlueprintCallable, Category = "Behavior")
+	int32 GetSelectedIndex() const { return SelectedIndex; }
+
+	/** Shift the current text left. */
+	UFUNCTION(BlueprintCallable, Category = "Behavior")
+	void ShiftTextLeft();
+
+	/** Shift the current text right. */
+	UFUNCTION(BlueprintCallable, Category = "Behavior")
+	void ShiftTextRight();
+
+public:
+
+	/** Called when the Selected state of this button changes */
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnSwitched OnSwitched;
+
+	DECLARE_EVENT_TwoParams(UCommonRotator, FSwitchedEvent, int32 /*Value*/, bool /*bUserInitiated*/);
+	FSwitchedEvent OnSwitchedEvent;
+
+protected:
+	void ShiftTextLeftInternal(bool bFromNavigation);
+	void ShiftTextRightInternal(bool bFromNavigation);
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category = Events, meta = (DisplayName = "On Options Populated"))
+	void BP_OnOptionsPopulated(int32 Count);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = Events, meta = (DisplayName = "On Options Selected"))
+	void BP_OnOptionSelected(int32 Index);
+
+protected:
+
+	/** The displayed text */
+	UPROPERTY(BlueprintReadOnly, Category = CommonRotator, Meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> LabelText;
+
+	// Holds the array of display texts
+	TArray<FText> TextLabels;
+
+	/** The index of the current text item */
+	int32 SelectedIndex;
+};
